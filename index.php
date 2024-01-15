c48f11833125189e68c8c29c58ed0332de828948
Pirogov <pirogovde@mer.ci.nsu.ru>
2024-01-15 16:49:56 +0700

    first working solution no frontend

--- index.php
+++ index.php
@@ -4,36 +4,81 @@ use core\Application;
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
-$apiKey = '';
-
-Introvert\Configuration::getDefaultConfiguration()->setApiKey('key', $apiKey);
-
 $api = new Introvert\ApiClient();
-try {
-    $result = $api->account->info();
-    print_r($result);
-} catch (Exception $e) {
-    echo 'Exception when calling account->allStatuses: ', $e->getMessage(), PHP_EOL;
-}
+$api->getConfig()->setHost('https://api.s1.yadrocrm.ru/tmp');
 
 
-function getAllSuccessfullLeads($dateFrom, $dateTo) {
-    $leads = getLeads();
-}
-function getInfo($api)
+function getAllSuccessfullLeads($dateFrom, $dateTo, $api)
 {
-    $result = $api->account->info();
-    return $result;
-}
-function getClients($api)
-{
-    $result = $api->yadro->getUsers();
-    return $result;
-}
-function getLeads($api)
-{
-    $result = $api->lead->getAll();
-    return $result;
+    $clients = getClients();
+    $filteredClients = filterClientsByAccess($clients, $api);
+
+    foreach ($filteredClients as $client) {
+        // set apiKey of current client
+        $api->getConfig()->setApiKey('key', $client["api"]);
+        $leads = [];
+        $offset = 0;
+        $limit = 100;
+        do {
+            // get limited amount of leads and push to resulting leads array
+            $leadsByPage = $api->lead->getAll($client["id"], 142, null, null, $limit, $offset);
+            $leads = array_merge_recursive($leads, $leadsByPage);
+            $count = count($leadsByPage["result"]);
+            $offset += $limit;
+        } while ($count >= $limit);
+        // Leads ready to be filtered by date
+
+        $filteredLeads = array_filter($leads["result"], fn($lead) => $lead["date_create"] > $dateFrom && $lead["date_close"] < $dateTo);
+        echo '<pre>';
+        foreach ($filteredLeads as $lead) {
+            var_dump($lead);
+        }
+        echo '</pre>';
+        $leadSum = array_reduce($filteredLeads, fn($sum, $lead) => $sum += $lead["price"]);
+        echo $leadSum;
+    }
+
 }
 
-getInfo($api);
+// Нужно отфильтровать клиентов на только активных
+
+function getClients()
+{
+    return [
+        [
+            "id" => 8967010,
+            "name" => "intrdev",
+            "api" => "23bc075b710da43f0ffb50ff9e889aed"
+        ],
+        [
+            "id" => 2,
+            "name" => "artedegrass0",
+            "api" => "",
+        ],
+    ];
+}
+
+function filterClientsByAccess($clients, $api)
+{
+    $filteredClients = [];
+    foreach ($clients as $client) {
+        $apiKey = $client['api'];
+        $api->getConfig()->setApiKey('key', $apiKey);
+        try {
+            $result = $api->account->info();
+            array_push($filteredClients, $client);
+        } catch (Exception $e) {
+
+        }
+    }
+    return $filteredClients;
+}
+
+function getClientLeads()
+{
+
+}
+
+$dateFrom = 1;
+$dateTo = 111111111111111111;
+echo getAllSuccessfullLeads($dateFrom, $dateTo, $api);
